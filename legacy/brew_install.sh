5761a4044ac1cb6b08695c47e9ce14f5127f2db6
eirikhanasand <eirik.hanasand@gmail.com>
2025-10-30 19:06:45 +0100

    Removed legacy code

--- legacy/brew_install.sh
+++
@@ -1,7 +0,0 @@
-brew install llvm libomp
-export PATH="/opt/homebrew/opt/llvm/bin:$PATH"
-export CMAKE_PREFIX_PATH=$(brew --prefix llvm)
-export LDFLAGS="-L/opt/homebrew/opt/libomp/lib"
-export CPPFLAGS="-I/opt/homebrew/opt/libomp/include"
-export CXXFLAGS="-I/opt/homebrew/opt/libomp/include"
-export CMAKE_PREFIX_PATH="/opt/homebrew/opt/llvm"
